10502b21bd3d5e37834fa0104093066cef34868a
Pilar <pilaransa31@gmail.com>
2024-09-09 14:55:30 -0300

    Fix: mejorando los mensajes de perdiste/ganaste

--- src/components/Jugar/Jugar.jsx
+++ src/components/Jugar/Jugar.jsx
@@ -1,12 +1,14 @@
 import { useState } from "react";
 import GameOver from "../Over/GameOver"
+import './Jugar.css'; 
+import Ganaste from "../Ganaste/Ganaste";
 
 
 
 const Jugar = () => {
     const [ intentos, setIntentos  ] = useState(10);
     const [ numElegido, setNumElegido ] = useState(0);
-    const [ perdiste, setPerdiste ] = useState(false);
+    const [ ganaste, setGanaste ] = useState(null);
     const numAAdivinar = 6;
     
 
@@ -25,13 +27,13 @@ const Jugar = () => {
       const num = parseInt(numElegido, 10);
       
       if(num === numAAdivinar){
-          console.log("GANASTE!")
+          setGanaste(true)
           console.log(numElegido);
-      } else if (num != numAAdivinar && intentos !== 0) {
+      } else if (num != numAAdivinar && intentos > 1) {
           console.log("Intenta de nuevo")
           setIntentos(intentos - 1)
-      } else if (intentos === 0) {
-        setPerdiste(true);
+      } else if (intentos === 1) {
+        setGanaste(false);
       }
   }
 
@@ -41,7 +43,8 @@ const Jugar = () => {
       <div>
         <h2>Escribí un número del 1 al 20</h2>
         <input type="number" value={numElegido} onChange={continuarJuego} onKeyDown={handleKeyDown}/>
-        <div ><GameOver { ...(perdiste === true ? {className: "noMostrar" }:{})}/></div> 
+        <div className={ganaste === false ? "estilo" : "noMostrar"}><GameOver /></div> 
+        <div className={ganaste === true ? "estilo" : "noMostrar"}><Ganaste/></div>
       </div >
     </div>
   )
